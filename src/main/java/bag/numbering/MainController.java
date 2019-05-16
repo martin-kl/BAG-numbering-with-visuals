c00cade57e9cfecd1c099501983e660909b02763
Martin Klampfer <e1526110@student.tuwien.ac.at>
2019-05-16 23:12:56 +0200

    data is now saved in json file and loaded from there - TODO: save dependencies (or not?)

--- src/main/java/bag/numbering/MainController.java
+++ src/main/java/bag/numbering/MainController.java
@@ -17,6 +17,8 @@ import javafx.stage.Stage;
 import javafx.util.Callback;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.*;
 
 public class MainController {
@@ -66,44 +68,171 @@ public class MainController {
     @FXML
     public TableColumn<Kapelle, String> clmEdit;
 
-    //Create all participants
-    Kapelle goellersdorf = new Kapelle("Blasmusikkapelle Göllersdorf", 311, 1, 5); //Festzelt(2)
-    Kapelle guntersdorf = new Kapelle("Trachtenkapelle Guntersdorf", 101);
-    Kapelle hadres = new Kapelle("Dorfmusik Hadres im Pulkautal", 345);
-    Kapelle hardegg = new Kapelle("Waldviertler Grenzlandkapelle Hardegg", 71);
-    //Kapelle heldenberg = new Kapelle("Jugend-Radetzkykapelle Heldenberg", 999, 1, PARTICIPANTS);
-    //Kapelle hollabrunn = new Kapelle("Stadtmusik Hollabrunn", 998, 1, PARTICIPANTS);
-    Kapelle mailberg = new Kapelle("Weinviertler Hauerkapelle Mailberg", 340, 4, 4); //wirklich 4. Kapelle?
-    Kapelle maissau = new Kapelle("Stadtmusik Maissau", 238, 1, 1);         //Gastkapelle
-    Kapelle obermarkersdorf = new Kapelle("Musikkapelle Obermarkersdorf", 248);
-    Kapelle pulkau = new Kapelle("Trachtenkapelle Pulkau", 187, 1, 6); //Festzelt(3)
-    Kapelle ravelsbach = new Kapelle("Jugend-Deutschmeisterkapelle Ravelsbach", 338);
-    Kapelle retz = new Kapelle("Stadtkapelle Retz", 278);
-    Kapelle retzbach = new Kapelle("Trachtenkapelle Retzbach", 191);
-    Kapelle roeschitz = new Kapelle("Musikverein Röschitz", 122);
-    //Kapelle roseldorf = new Kapelle("Musikkapelle Roseldorf", 378, 1, PARTICIPANTS);
-    Kapelle schmidatal = new Kapelle("Musikverein Schmidatal & Musikkapelle Roseldorf", 154); //2018 gemeinsam angetreten
-    Kapelle theras = new Kapelle("Trachtenkapelle Theras", 245);
-    Kapelle unterduernbach = new Kapelle("Musikverein Unterdürnbach", 997);
-    Kapelle wullersdorf = new Kapelle("Jugend-Musikverein Wullersdorf", 435);
-    Kapelle zellerndorf = new Kapelle("Musikkapelle Zellerndorf", 170, 1, 2); //Festzelt(1)
-    Kapelle ziersdorf = new Kapelle("Trachtenkapelle Ziersdorf und Umgebung", 369, 1, 8);
-    Kapelle kirchberg = new Kapelle("Musikverein Kirchberg am Wagram", 999);
-    Kapelle angerberg_mariastein = new Kapelle("Bundesmusikkapelle Angerberg/Mariastein", 999, 10, 0);
-
-    private final ArrayList<Kapelle> allKapellen = new ArrayList<>(Arrays.asList(goellersdorf, guntersdorf, hadres,
-            hardegg, mailberg, maissau, obermarkersdorf, pulkau, ravelsbach, retz, retzbach, roeschitz, schmidatal,
-            theras, unterduernbach, wullersdorf, zellerndorf, ziersdorf, kirchberg, angerberg_mariastein));
+    private ArrayList<Kapelle> allKapellen;
 
     public void initialize() {
-        /*
-        vbResult.getChildren().clear();
-        vbResult.getChildren().add(bpSettings);
-        */
         clearVbOuter();
         //add settings at the beginning
         vbOuter.getChildren().add(bpSettings);
 
+        //load data from file
+        try {
+            allKapellen = Utils.readFromFile();
+        } catch (IOException e) {
+            showExceptionDialog(e,
+                    "Fehler",
+                    "Fehler beim Laden",
+                    "Fehler beim Laden der Daten aus der Datei " + Utils.FILE_PATH + ".\n" +
+                            "Siehe Meldung für mehr Details:");
+        }
+
+        //and init the whole table
+        initTable();
+    }
+
+    private void clearVbOuter() {
+        vbOuter.getChildren().clear();
+        vbOuter.getChildren().add(hbHeader);
+        vbOuter.getChildren().add(hbButtons);
+    }
+
+    public void openSettings(ActionEvent actionEvent) {
+        //vbResult.getChildren().clear();
+        //vbResult.getChildren().add(bpSettings);
+        clearVbOuter();
+        vbOuter.getChildren().add(bpSettings);
+    }
+
+
+    public void startAlgorithmNormally(ActionEvent actionEvent) {
+        ArrayList<Kapelle> participants = Utils.deepCopyOfActiveOnes(allKapellen);
+        if (!isDataValid(participants)) {
+            return;
+        }
+        //refresh so that all calculated values are now seen in the table
+        tblSettings.refresh();
+
+        switchToOutput();
+
+        StNrAlgorithm stNrAlgorithm = getInitializedStrNrAlgorithmInstance(participants);
+
+        //stNrAlgorithm.startAlgorithmOnce(true, false);
+        boolean result = stNrAlgorithm.startAlgorithmLoop(false);
+
+        addResultToGraphicOutput(true, stNrAlgorithm.getResult(), result);
+        if (!result) {
+            showAlert(Alert.AlertType.ERROR, "Fehler", "Zuweisung fehlgeschlagen",
+                    "Konnte selbst nach " + StNrAlgorithm.MAX_TRIES + " Versuchen keine Zurodnung finden!");
+        }
+    }
+
+    public void startAlgorithmLoop(ActionEvent actionEvent) {
+        ArrayList<Kapelle> participants = Utils.deepCopyOfActiveOnes(allKapellen);
+        if (!isDataValid(participants)) {
+            return;
+        }
+        //refresh so that all calculated values are now seen in the table
+        tblSettings.refresh();
+
+        switchToOutput();
+
+        StNrAlgorithm stNrAlgorithm = getInitializedStrNrAlgorithmInstance(participants);
+
+        int posCounter = 0;
+        boolean lastResult;
+        for (int i = 0; i < LOOP_TIMES - 1; i++) {
+            //System.out.println("\t\tDurchlauf Nummer = " + (i + 1));
+            if (stNrAlgorithm.startAlgorithmOnce(false, true)) {
+                posCounter++;
+            }
+        }
+        //System.out.println("\n\t\tDurchlauf Nummer = " + LOOP_TIMES);
+        if ((lastResult = stNrAlgorithm.startAlgorithmOnce(true, true))) {
+            posCounter++;
+        }
+
+        lbLoopTimes.setText("" + LOOP_TIMES);
+        lbSuccessful.setText("" + posCounter);
+        lbResult.setText((LOOP_TIMES - posCounter) + " Zuweisungen sind nicht gelungen!");
+        //spResults.setContent(gridPaneStatistical);
+        vbResult.getChildren().clear();
+        vbResult.getChildren().add(gridPaneStatistical);
+
+        vbResult.getChildren().add(new Separator());
+        vbResult.getChildren().add(new Label(""));
+        Label tempLabel = new Label("Ergebnis der letzten Simulation:");
+        tempLabel.setStyle("-fx-font: 13pt System;");
+        vbResult.getChildren().add(tempLabel);
+        vbResult.getChildren().add(new Label(""));
+
+        addResultToGraphicOutput(false, stNrAlgorithm.getResult(), lastResult);
+        if (LOOP_TIMES - posCounter == LOOP_TIMES) {
+            showAlert(Alert.AlertType.ERROR, "Fehler", "Alle Zuweisungen fehlgeschlagen",
+                    "Alle versuchten " + LOOP_TIMES + " Zuweisungen sind fehlgeschlagen!");
+        }
+    }
+
+    private void switchToOutput() {
+        clearVbOuter();
+        vbOuter.getChildren().add(scrollPane);
+    }
+
+
+    private boolean isDataValid(ArrayList<Kapelle> participants) {
+        String res = Utils.pruefeStNr(participants);
+        if (!res.equals("")) {
+            showAlert(Alert.AlertType.ERROR, "Fehler", "Daten nicht valide", res);
+            return false;
+        }
+
+        res = Utils.pruefeAbhaengigkeit(dependencies);
+        if (!res.equals("")) {
+            showAlert(Alert.AlertType.ERROR, "Fehler", "Daten nicht valide", res);
+            return false;
+        }
+        return true;
+    }
+
+    private void showAlert(Alert.AlertType type, String title, String headerText, String content) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    private StNrAlgorithm getInitializedStrNrAlgorithmInstance(ArrayList<Kapelle> participants) {
+        Map<Kapelle, ArrayList<Kapelle>> participatingDependencies = new HashMap<>();
+        for (Kapelle kap : dependencies.keySet()) {
+            if (kap.isActive())
+                participatingDependencies.put(kap, dependencies.get(kap));
+        }
+        StNrAlgorithm stNrAlgorithm = new StNrAlgorithm(participants, participatingDependencies);
+        stNrAlgorithm.init();
+        return stNrAlgorithm;
+    }
+
+    private void addResultToGraphicOutput(boolean deleteList, Kapelle[] result, boolean resultStatus) {
+        if (deleteList) {
+            vbResult.getChildren().clear();
+        }
+        if (resultStatus) {
+            for (int i = 1; i < result.length; i++) {
+                Label tempLabel = new Label(i + ":\t\t" + result[i].getBez());
+                tempLabel.setStyle("-fx-font: 13pt System;");
+                vbResult.getChildren()
+                        .add(tempLabel);
+            }
+        } else {
+            Label tempLabel = new Label("Zuweisung nicht gelungen");
+            tempLabel.setStyle("-fx-font: 13pt System;");
+            vbResult.getChildren()
+                    .add(tempLabel);
+        }
+    }
+
+
+    private void initTable() {
         //initialize table
 
         //setting the table to editable is needed but done in the fxml file
@@ -192,144 +321,50 @@ public class MainController {
         tblSettings.setItems(data);
     }
 
-    private void clearVbOuter() {
-        vbOuter.getChildren().clear();
-        vbOuter.getChildren().add(hbHeader);
-        vbOuter.getChildren().add(hbButtons);
-    }
-
-    public void openSettings(ActionEvent actionEvent) {
-        //vbResult.getChildren().clear();
-        //vbResult.getChildren().add(bpSettings);
-        clearVbOuter();
-        vbOuter.getChildren().add(bpSettings);
-    }
-
-
-    public void startAlgorithmNormally(ActionEvent actionEvent) {
-        ArrayList<Kapelle> participants = Utils.deepCopyOfActiveOnes(allKapellen);
-        if (!isDataValid(participants)) {
-            return;
-        }
-        //refresh so that all calculated values are now seen in the table
-        tblSettings.refresh();
-
-        switchToOutput();
-
-        StNrAlgorithm stNrAlgorithm = getInitializedStrNrAlgorithmInstance(participants);
-
-        //stNrAlgorithm.startAlgorithmOnce(true, false);
-        boolean result = stNrAlgorithm.startAlgorithmLoop(false);
-
-        addResultToGraphicOutput(true, stNrAlgorithm.getResult(), result);
-        if (!result) {
-            showErrorAlert("Zuweisung fehlgeschlagen",
-                    "Konnte selbst nach " + StNrAlgorithm.MAX_TRIES + " Versuchen keine Zurodnung finden!");
+    public void saveData(ActionEvent actionEvent) {
+        try {
+            Utils.writeToFile(data);
+            showAlert(Alert.AlertType.INFORMATION, "Erfolgreich", "Erfolgreich gespeichert",
+                    "Daten wurden erfolgreich gespeichert.");
+        } catch (IOException e) {
+            showExceptionDialog(e,
+                    "IO - Fehler",
+                    "Fehler beim Speichern der Daten",
+                    "Konnte Daten nicht unter " + Utils.FILE_PATH + " speichern.");
         }
     }
 
-    public void startAlgorithmLoop(ActionEvent actionEvent) {
-        ArrayList<Kapelle> participants = Utils.deepCopyOfActiveOnes(allKapellen);
-        if (!isDataValid(participants)) {
-            return;
-        }
-        //refresh so that all calculated values are now seen in the table
-        tblSettings.refresh();
-
-        switchToOutput();
-
-        StNrAlgorithm stNrAlgorithm = getInitializedStrNrAlgorithmInstance(participants);
-
-        int posCounter = 0;
-        boolean lastResult;
-        for (int i = 0; i < LOOP_TIMES - 1; i++) {
-            //System.out.println("\t\tDurchlauf Nummer = " + (i + 1));
-            if (stNrAlgorithm.startAlgorithmOnce(false, true)) {
-                posCounter++;
-            }
-        }
-        //System.out.println("\n\t\tDurchlauf Nummer = " + LOOP_TIMES);
-        if ((lastResult = stNrAlgorithm.startAlgorithmOnce(true, true))) {
-            posCounter++;
-        }
-
-        lbLoopTimes.setText("" + LOOP_TIMES);
-        lbSuccessful.setText("" + posCounter);
-        lbResult.setText((LOOP_TIMES - posCounter) + " Zuweisungen sind nicht gelungen!");
-        //spResults.setContent(gridPaneStatistical);
-        vbResult.getChildren().clear();
-        vbResult.getChildren().add(gridPaneStatistical);
-
-        vbResult.getChildren().add(new Separator());
-        vbResult.getChildren().add(new Label(""));
-        Label tempLabel = new Label("Ergebnis der letzten Simulation:");
-        tempLabel.setStyle("-fx-font: 13pt System;");
-        vbResult.getChildren().add(tempLabel);
-        vbResult.getChildren().add(new Label(""));
-
-        addResultToGraphicOutput(false, stNrAlgorithm.getResult(), lastResult);
-        if (LOOP_TIMES - posCounter == LOOP_TIMES) {
-            showErrorAlert("Alle Zuweisungen fehlgeschlagen", "Alle versuchten " + LOOP_TIMES + "" +
-                    " Zuweisungen sind fehlgeschlagen!");
-        }
-    }
-
-    private void switchToOutput() {
-        clearVbOuter();
-        vbOuter.getChildren().add(scrollPane);
-    }
-
-
-    private boolean isDataValid(ArrayList<Kapelle> participants) {
-        String res = Utils.pruefeStNr(participants);
-        if (!res.equals("")) {
-            showErrorAlert("Daten nicht valide", res);
-            return false;
-        }
-
-        res = Utils.pruefeAbhaengigkeit(dependencies);
-        if (!res.equals("")) {
-            showErrorAlert("Daten nicht valide", res);
-            return false;
-        }
-        return true;
-    }
-
-    private void showErrorAlert(String headerText, String content) {
+    private void showExceptionDialog(Exception e, String title, String header, String content) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle("Fehler");
-        alert.setHeaderText(headerText);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
         alert.setContentText(content);
+
+        // Create expandable Exception.
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        String exceptionText = sw.toString();
+
+        Label label = new Label("The exception stacktrace was:");
+
+        TextArea textArea = new TextArea(exceptionText);
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(label, 0, 0);
+        expContent.add(textArea, 0, 1);
+
+        // Set expandable Exception into the dialog pane.
+        alert.getDialogPane().setExpandableContent(expContent);
+
         alert.showAndWait();
-    }
-
-    private StNrAlgorithm getInitializedStrNrAlgorithmInstance(ArrayList<Kapelle> participants) {
-        Map<Kapelle, ArrayList<Kapelle>> participatingDependencies = new HashMap<>();
-        for (Kapelle kap : dependencies.keySet()) {
-            if (kap.isActive())
-                participatingDependencies.put(kap, dependencies.get(kap));
-        }
-        StNrAlgorithm stNrAlgorithm = new StNrAlgorithm(participants, participatingDependencies);
-        stNrAlgorithm.init();
-        return stNrAlgorithm;
-    }
-
-    private void addResultToGraphicOutput(boolean deleteList, Kapelle[] result, boolean resultStatus) {
-        if (deleteList) {
-            vbResult.getChildren().clear();
-        }
-        if (resultStatus) {
-            for (int i = 1; i < result.length; i++) {
-                Label tempLabel = new Label(i + ":\t\t" + result[i].getBez());
-                tempLabel.setStyle("-fx-font: 13pt System;");
-                vbResult.getChildren()
-                        .add(tempLabel);
-            }
-        } else {
-            Label tempLabel = new Label("Zuweisung nicht gelungen");
-            tempLabel.setStyle("-fx-font: 13pt System;");
-            vbResult.getChildren()
-                    .add(tempLabel);
-        }
     }
 }
--- src/main/java/bag/numbering/Utils.java
+++ src/main/java/bag/numbering/Utils.java
@@ -1,10 +1,22 @@
 package bag.numbering;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class Utils {
+
+    public static final String FILE_PATH = "./data.json";
+    private static final Type KAPELLEN_TYPE = new TypeToken<List<Kapelle>>() {
+    }.getType();
 
     public static String pruefeStNr(List<Kapelle> list) {
         //first count active participants to be able to set latest starting number
@@ -90,11 +102,56 @@ public class Utils {
             }
         }
         //now look in the other direction: see if every lastUpdatedDep is also in the other ones
-        for(Kapelle dep : lastUpdatedDep) {
+        for (Kapelle dep : lastUpdatedDep) {
             dependencies.computeIfAbsent(dep, kl -> new ArrayList<>());
-            if(!dependencies.get(dep).contains(lastUpdated)) {
+            if (!dependencies.get(dep).contains(lastUpdated)) {
                 dependencies.get(dep).add(lastUpdated);
             }
         }
+    }
+
+    public static void writeToFile(List<Kapelle> data) throws IOException {
+        Gson gson = new Gson();
+        FileWriter writer = new FileWriter(FILE_PATH);
+        gson.toJson(data, writer);
+        writer.flush();
+        writer.close();
+    }
+
+    public static ArrayList<Kapelle> readFromFile() throws IOException {
+        Gson gson = new Gson();
+        FileReader reader = new FileReader(FILE_PATH);
+        return gson.fromJson(reader, KAPELLEN_TYPE);
+    }
+
+    public List<Kapelle> getAllParticipantsOLD() {
+        //Create all participants
+        Kapelle goellersdorf = new Kapelle("Blasmusikkapelle Göllersdorf", 311, 1, 5); //Festzelt(2)
+        Kapelle guntersdorf = new Kapelle("Trachtenkapelle Guntersdorf", 101);
+        Kapelle hadres = new Kapelle("Dorfmusik Hadres im Pulkautal", 345);
+        Kapelle hardegg = new Kapelle("Waldviertler Grenzlandkapelle Hardegg", 71);
+        //Kapelle heldenberg = new Kapelle("Jugend-Radetzkykapelle Heldenberg", 999, 1, PARTICIPANTS);
+        //Kapelle hollabrunn = new Kapelle("Stadtmusik Hollabrunn", 998, 1, PARTICIPANTS);
+        Kapelle mailberg = new Kapelle("Weinviertler Hauerkapelle Mailberg", 340, 4, 4); //wirklich 4. Kapelle?
+        Kapelle maissau = new Kapelle("Stadtmusik Maissau", 238, 1, 1);         //Gastkapelle
+        Kapelle obermarkersdorf = new Kapelle("Musikkapelle Obermarkersdorf", 248);
+        Kapelle pulkau = new Kapelle("Trachtenkapelle Pulkau", 187, 1, 6); //Festzelt(3)
+        Kapelle ravelsbach = new Kapelle("Jugend-Deutschmeisterkapelle Ravelsbach", 338);
+        Kapelle retz = new Kapelle("Stadtkapelle Retz", 278);
+        Kapelle retzbach = new Kapelle("Trachtenkapelle Retzbach", 191);
+        Kapelle roeschitz = new Kapelle("Musikverein Röschitz", 122);
+        //Kapelle roseldorf = new Kapelle("Musikkapelle Roseldorf", 378, 1, PARTICIPANTS);
+        Kapelle schmidatal = new Kapelle("Musikverein Schmidatal & Musikkapelle Roseldorf", 154); //2018 gemeinsam angetreten
+        Kapelle theras = new Kapelle("Trachtenkapelle Theras", 245);
+        Kapelle unterduernbach = new Kapelle("Musikverein Unterdürnbach", 997);
+        Kapelle wullersdorf = new Kapelle("Jugend-Musikverein Wullersdorf", 435);
+        Kapelle zellerndorf = new Kapelle("Musikkapelle Zellerndorf", 170, 1, 2); //Festzelt(1)
+        Kapelle ziersdorf = new Kapelle("Trachtenkapelle Ziersdorf und Umgebung", 369, 1, 8);
+        Kapelle kirchberg = new Kapelle("Musikverein Kirchberg am Wagram", 999);
+        Kapelle angerberg_mariastein = new Kapelle("Bundesmusikkapelle Angerberg/Mariastein", 999, 10, 0);
+
+        return new ArrayList<>(Arrays.asList(goellersdorf, guntersdorf, hadres,
+                hardegg, mailberg, maissau, obermarkersdorf, pulkau, ravelsbach, retz, retzbach, roeschitz, schmidatal,
+                theras, unterduernbach, wullersdorf, zellerndorf, ziersdorf, kirchberg, angerberg_mariastein));
     }
 }
